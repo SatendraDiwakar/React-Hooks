05d1034f88ba678b93b4a3211aa8c2c0fb04593c
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-11-22 22:45:05 +0530

    Explained useState hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+// components
+import UseStateHook from './hooks/UseStateHook';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ textDecoration: 'underline' }}>React Hooks</h1>
+      {/* 1st hook -> useState */}
+      <UseStateHook />
     </div>
   );
 }
---
+++ src/hooks/UseStateHook.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+export default function UseStateHook() {
+
+    // This component is used to explain how we can use useState hook
+    // to create state
+    // states
+    const [stateHook, setStateHook] = useState(0);
+    // This will return a state with initial value "0" and 
+    // a function to change the state with some value.
+
+    return (
+        <div>
+            {/* using value of stateHook */}
+            <h3>{stateHook}</h3>
+            {/* Incrementing value of stateHook using setStateHook function */}
+            <button style={{ width: '1.8em' }}
+                onClick={() => setStateHook(preVal => preVal + 1)}
+            >+</button>
+            {/* Decrementing value of stateHook using setStateHook function */}
+            <button style={{ width: '1.8em', margin: '0 .5em' }}
+                onClick={() => setStateHook(preVal => preVal - 1)}
+            >-</button>
+        </div>
+    )
+}
